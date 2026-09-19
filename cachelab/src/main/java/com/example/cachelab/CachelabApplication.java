50cb4b2e8514537ee5d6fdbe378ee498377b38e6
Niro-dev91 <npothupitiya.gitlab@gmail.com>
2026-09-19 20:36:26 +0530

    feat: initialize distributed cache lab

---
+++ cachelab/src/main/java/com/example/cachelab/CachelabApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachelabApplication.class, args);
+	}
+
+}
---
+++ cachelab/src/test/java/com/example/cachelab/CachelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
